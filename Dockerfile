d7bc78207d3ea0e996414c1092fa9b668759e766
Yash Singh <yashs33244@gmail.com>
2024-06-26 11:54:07 +0530

    some new changes

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,29 @@
 FROM node:20
- 
+
+# Set the working directory
 WORKDIR /usr/src/app
- 
+
+# Install ffmpeg
+RUN apt-get update && apt-get install -y ffmpeg
+
 # Copy root package.json and lockfile
 COPY package.json ./
 COPY package-lock.json ./
- 
+
 # Copy the api package.json
 COPY apps/videoProcessingService/package.json ./apps/videoProcessingService/package.json
 
-RUN apt-get update && apt-get install -y ffmpeg
+# Install dependencies
 RUN npm install
- 
+
 # Copy app source
 COPY . .
- 
-EXPOSE 3000
- 
-CMD [ "npm","run", "start-videoProcessingService" ]
+
+# Ensure the application listens on port 8080
+ENV PORT 8080
+
+# Expose the port the app runs on
+EXPOSE 8080
+
+# Start the application
+CMD [ "npm", "run", "start-videoProcessingService" ]
--- apps/videoProcessingService/src/index.ts
+++ apps/videoProcessingService/src/index.ts
@@ -1,31 +1,47 @@
 import express  from "express";
 import ffmpeg from "fluent-ffmpeg";
+import { convertVideo, deleteProcessedVideo,deleteRawVideo, downloadRawVideo, setupDirectories, uploadProcessedVideo } from "./storage";
+
+setupDirectories();
 
 const app = express();
 app.use(express.json());    
 
-app.post("/process-video", (req, res)=>{
-    const inputFilePath = req.body.inputFilePath;
-    const outputFilePath = req.body.outputFilePath;
-
-    if(!inputFilePath || !outputFilePath){
-        res.status(400).send("Missing input or output file path");
+app.post("/process-video", async (req, res)=>{
+    let data;
+    try{
+        const message = Buffer.from(req.body.message.data, "base64").toString('utf-8'); 
+        data = JSON.parse(message);
+        if(!data.name){
+            throw new Error("Invalid message payload is recieved"); 
+        }   
+    }catch(error){
+        console.error(error);
+        return res.status(400).send("Bad request: missing filename");    
     }
-    ffmpeg(inputFilePath)
-        .outputOptions("-vf", "scale=-2:360") // 360p
-        .on("end", ()=>{        
-            res.status(200).send("Video processing completed");
-        })   
-        .on("error", (err)=>{
-            console.error(err);
-            res.status(500).send("Video processing failed internal server error");
-        })
-        .save(outputFilePath);    
 
+    const inputFileName = data.name;    
+    const outputFilePath = `processed-${inputFileName}`;   
+    
+    await downloadRawVideo(inputFileName);
+    // first process then upload
+    try{
+        await convertVideo(inputFileName, outputFilePath);
+    }catch(error){
+        await Promise.all([
+            deleteRawVideo(inputFileName),
+            deleteProcessedVideo(outputFilePath)
+        ]);
+        console.log(error);
+        return res.status(500).send("Internal server error: Video processing failed");   
+    }
+
+    await uploadProcessedVideo(outputFilePath);
+    res.status(200).send("Processing finished successfully  ");
 });
 
 
-const port = process.env.PORT || 3000;  
+const port = process.env.PORT || 8080;  
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);  
 });
