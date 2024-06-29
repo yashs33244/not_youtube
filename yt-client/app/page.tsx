d1c476d38354a7c222076a01e9fe7dabb06c0e7a
Yash Singh <yashs33244@gmail.com>
2024-06-29 15:48:41 +0530

    right backend push

--- yt-client/app/page.tsx
+++ yt-client/app/page.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
+
 
 export default function Home() {
   return (
     <div>
       this is the home page
-      
+
     </div>
   );
 }
---
+++ yt-client/components/Navbar.tsx
@@ -0,0 +1,8 @@
+
+export function Navbar(){
+    return (
+        <main>
+            this is the navbar
+        </main>
+    )
+}
