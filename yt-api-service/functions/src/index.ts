ebd1796d33e8edfad5d32dcf27f142f2080a3023
Yash Singh <yashs33244@gmail.com>
2024-07-03 18:05:41 +0530

    new code 1

--- yt-api-service/functions/src/index.ts
+++ yt-api-service/functions/src/index.ts
@@ -1,10 +1,15 @@
-import * as logger from "firebase-functions/logger";
 import * as functions from "firebase-functions";
 import {initializeApp} from "firebase-admin/app";
 import {Firestore} from "firebase-admin/firestore";
+import * as logger from "firebase-functions/logger";
+import {Storage} from "@google-cloud/storage";
+import {onCall} from "firebase-functions/v2/https";
 
 initializeApp();
 const firestore = new Firestore();
+const storage = new Storage();
+
+const rawVideoBucketName = "yash_yt_raw_videos"; // must be unique globally
 
 export const createUser = functions.auth.user().onCreate((user)=>{
   const userInfo = {
@@ -19,4 +24,29 @@ export const createUser = functions.auth.user().onCreate((user)=>{
     .catch((error) => {
       logger.error(`Error creating user: ${error.message}`);
     });
+});
+
+
+export const generateUploadUrl = onCall({maxInstances: 1}, async (request)=>{
+  // check if user is authenticated
+  if (!request.auth) {
+    throw new functions.https.HttpsError(
+      "unauthenticated",
+      "User must be authenticated"
+    );
+  }
+
+  const auth = request.auth;
+  const data = request.data;
+  const bucket = storage.bucket(rawVideoBucketName);
+
+  // generate a unique filename for the video
+  // <uid>
+  const fileName = `${auth.uid}-${Date.now()}.${data.fileExtension}`;
+  const [url] = await bucket.file(fileName).getSignedUrl({
+    version: "v4",
+    action: "write",
+    expires: Date.now() + 15 * 60 * 1000, // 15 minutes
+  });
+  return {url, fileName};
 });
--- yt-client/components/Navbar.tsx
+++ yt-client/components/Navbar.tsx
@@ -1,15 +1,17 @@
 "use client"
 
-import { use, useState, useEffect } from 'react';
+import React, { use, useState, useEffect } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import SignIn from './SignIn';
 import { User } from 'firebase/auth';   
 import { onAuthStateChangedHelper } from '@/app/firebase/firebase';
+import { uploadVideo } from '@/app/firebase/functions';
 
 export function Navbar() {
     const [searchQuery, setSearchQuery] = useState('');
     const [user, setUser] = useState<User | null>(null);
+    const [selectedFile, setSelectedFile] = useState<File | null>(null);
 
     useEffect(() => {
         const unsubscribe = onAuthStateChangedHelper(user =>{
@@ -21,6 +23,23 @@ export function Navbar() {
         e.preventDefault();
         // Implement search functionality here
         console.log('Searching for:', searchQuery);
+    };
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.item(0);
+        if (file) {
+            setSelectedFile(file);
+            handleUpload(file);
+        }
+    };
+
+    const handleUpload = async (file: File) => {
+        try {
+            const response = await uploadVideo(file);
+            alert(`Video uploaded successfully with response: ${JSON.stringify(response)}`);
+        } catch (error) {
+            console.error('Error uploading video:', error);
+            alert('Error uploading video');
+        }
     };
 
     return (
@@ -52,11 +71,18 @@ export function Navbar() {
             </div>
             
             <div className="flex items-center">
-                <button className="p-2 hover:bg-[#222222] rounded-full">
-                    <svg viewBox="0 0 24 24" width="24" height="24" fill="currentColor">
-                        <path d="M14,13h-3v3H9v-3H6v-2h3V8h2v3h3V13z M17,6H3v12h14v-6.39l4,1.83V8.56l-4,1.83V6 M18,5v3.83L22,7v8l-4-1.83V19H2V5H18L18,5 z"></path>
-                    </svg>
-                </button>
+                {user && <label className="p-2 hover:bg-[#222222] rounded-full cursor-pointer">
+                            <input
+                                type="file"
+                                className="hidden"
+                                accept="video/*"
+                                onChange={handleFileChange}
+                            />
+                            <svg viewBox="0 0 24 24" width="24" height="24" fill="currentColor">
+                                <path d="M14,13h-3v3H9v-3H6v-2h3V8h2v3h3V13z M17,6H3v12h14v-6.39l4,1.83V8.56l-4,1.83V6 M18,5v3.83L22,7v8l-4-1.83V19H2V5H18L18,5 z"></path>
+                            </svg>
+                        </label>
+                }
                 <button className="p-2 ml-2 hover:bg-[#222222] rounded-full">
                     <svg viewBox="0 0 24 24" width="24" height="24" fill="currentColor">
                         <path d="M10,20h4c0,1.1-0.9,2-2,2S10,21.1,10,20z M20,17.35V19H4v-1.65l2-1.88v-5.15c0-2.92,1.56-5.22,4-5.98V3.96 c0-1.42,1.49-2.5,2.99-1.76C13.64,2.52,14,3.23,14,3.96l0,0.39c2.44,0.75,4,3.06,4,5.98v5.15L20,17.35z M19,17.77l-2-1.88v-5.47 c0-2.47-1.19-4.36-3.13-5.1c-1.26-0.53-2.64-0.5-3.84,0.03C8.15,6.11,7,7.99,7,10.42v5.47l-2,1.88V18h14V17.77z"></path>
